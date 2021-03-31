4d508a092aefcdc47d57972410e87bc5d7702d63
Spasmochi <spasmochi@gmail.com>
2021-03-31 18:12:44 +1100

    added my solution

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,20 @@
 const { httpGet } = require('./mock-http-interface');
 
+const statusMap = {
+  200: "Arnie Quote",
+  500: "FAILURE"
+}
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  try {
+    const promises = urls.map(httpGet)
+    const results = await Promise.all(promises)
+    return results.map(res => ({
+      [statusMap[res.status]]: JSON.parse(res.body).message
+    }))
+  } catch (error) {
+    console.log(error)
+  }
 };
 
 module.exports = {
